17303e65447d6e2cca8b26a393bd3fd9217448e9
gazorpazorp85 <paolo.groppi@gmail.com>
2020-03-12 17:54:06 +0200

    removed hover on big icons

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Switch>
         <Route path="/" component={Home} exact />
       </Switch>
